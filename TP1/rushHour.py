9b166e535ee14675f8cb5eaed1f4911763892571
alePereira <alexandre.pereira@polymtl.ca>
2017-01-25 17:04:33 -0500

    traduction of rushHour.java in python

--- TP1/rushHour.py
+++ TP1/rushHour.py
@@ -1,2 +1,46 @@
+import priorityQueue
+
 class RushHour(object):
-    pass
+    
+    def __init__(self):
+        """
+            definition de la grille
+        """
+
+        self.nbcars = 0
+        self.color = []
+        self.horiz = []
+        self.len = []
+        self.moveon = []
+        self.free = [[False] * 6 for _ in range(6)]
+
+    def initFree(self, state):
+        # TODO
+
+
+    def moves(self, state):
+        initFree(state)
+        l = []
+        # TODO
+        return l
+
+    def solve(self,state):
+        visited = [state]
+        q = []
+        # TODO
+        print("pas de solution")
+        return None
+    
+    def solveAstar(self,state):
+        visited = [state]
+        q = priorityQueue.PriorityQueue()
+        # TODO
+        print("pas de solution")
+        return None
+
+
+    def printSolution(self,state):
+        # TODO
+
+        
+
